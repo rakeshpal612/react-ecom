e9be27039e06c81dbc628341f11f7e89b2990c5e
Rakesh Pal <rkpal8097@gmail.com>
2024-02-06 22:35:09 +0530

    minor change in product list

--- src/features/product-list/ProductList.jsx
+++ src/features/product-list/ProductList.jsx
@@ -217,7 +217,7 @@ export default function ProductList() {
           <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-12">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900">
-                All Products
+                All Product
               </h1>
 
               <div className="flex items-center">
